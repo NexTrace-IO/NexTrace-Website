f0ef38ae6f1f6086d73dfacd2dcaf0070db56f89
Connor Keyes <connorkeyes@Connors-MacBook-Air.local>
2023-10-25 15:23:00 -0700

    Add features to feature component

--- nextrace-website/src/app/components/Feature.tsx
+++ nextrace-website/src/app/components/Feature.tsx
@@ -19,13 +19,13 @@ const features = [
     icon: LockClosedIcon,
   },
   {
-    name: 'Simple queues.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+    name: 'Real-time analysis.',
+    description: 'Metrics render your application\'s observability metrics instantly as it runs.',
     icon: ArrowPathIcon,
   },
   {
     name: 'Advanced security.',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    description: 'Stopping a session de-instruments your application and keeps all your changes.',
     icon: FingerPrintIcon,
   },
   {
@@ -34,8 +34,8 @@ const features = [
     icon: Cog6ToothIcon,
   },
   {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+    name: 'Secure state.',
+    description: 'NexTrace preserves the state of your extension session so you don\'t miss anything.',
     icon: ServerIcon,
   },
 ]
@@ -48,7 +48,7 @@ export default function Feature() {
         
         <div className="mx-auto max-w-2xl sm:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No problem.</p>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Server. Client. All in one.</p>
           <p className="mt-6 text-lg leading-8 text-gray-300">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
             dolor cupiditate blanditiis.
