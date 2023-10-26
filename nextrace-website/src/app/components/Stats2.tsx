10e290cad6efb847fbff572665bb50d2716409ad
Jose Velasco <josevelasco1022@gmail.com>
2023-10-26 10:32:02 -0700

    console log component

--- nextrace-website/src/app/components/Stats2.tsx
+++ nextrace-website/src/app/components/Stats2.tsx
@@ -2,37 +2,36 @@ import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/2
 
 const features = [
   {
-    name: 'Push to deploy.',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    name: '1',
+    description: 'Real-time monitoring of console logs for quick issue identification.',
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
+    name: '2',
+    description: 'Instant single-click navigation to log-related code, enhancing debugging efficiency.',
+    icon: CloudArrowUpIcon,
   },
   {
-    name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
-    icon: ServerIcon,
+    name: '3',
+    description: 'Seamlessly supports both client and server-side log rendering, simplifying troubleshooting across the application.',
+    icon: CloudArrowUpIcon,
   },
 ]
 
 export default function Stats2() {
   return (
-    <div className="bg-gray-900 py-24">
+    <div className="bg-gray-900">
       <div className="mx-auto max-w-8xl sm:px-6 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-20 sm:rounded-3xl sm:px-10 sm:py-24 lg:py-24 xl:px-24">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
             <div className="lg:row-start-2 lg:max-w-md">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
+              Instant Console Log Navigation Tool
                 <br />
-                Start using our app today.
+                
               </h2>
               <p className="mt-6 text-lg leading-8 text-gray-300">
-                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
-                Ac euismod vel sit maecenas.
+              This tool offers real-time console log display and instant navigation for easier debugging of client and server-side code.
               </p>
             </div>
             <img
